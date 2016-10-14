d33d1602dc881479ec3e1fe80da8fac3ba9344a2
aburgos <aburgos@globosat.com.br>
2016-10-14 17:17:22 -0300

    V0.1

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get <- function() x
+  setinversion <- function(inversion) i<<-inversion
+  getinversion <- function() i
+  list(set = set, get = get,
+       setinversion = setinversion, getinversion = getinversion)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinversion()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinversion(i)
+  i
 }
+
+
+#source("cachematrix.R")
+#my_matrix <- makeCacheMatrix( matrix(c(4,3,3,2),2,2))
+#cacheSolve(my_matrix)
