95eef23f77128407f8b8c31c956af5d14972fbb4
ahbleite <ahbleite@gmail.com>
2016-10-15 12:44:36 -0300

    Adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  These functions create an special matrix that can calculate and cache its own inverse 
 
-## Write a short comment describing this function
+## Function Name:
+## Description: Creates a special "matrix" object that can cache its own inverse
+
+##  Function:
+##    makeCacheMatrix <- function(x = matrix())
+##  Args:
+##    x: an invertible matrix
+##  Description: Returns the inverse of a matrix from the cache, 
+##    Creates a special "matrix" object that can cache its own inverse
+##  Returns:
+##    The inverse of matrix x
+##  Sample: 
+##    source("cachematrix.R")
+##    my_matrix <- makeCacheMatrix( matrix(c(4,3,3,2),2,2))
+##    cacheSolve(my_matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -17,8 +30,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+##  Function:
+##    cacheSolve <- function(x, ...)
+##  Args:
+##    x: an "special" matrix
+##  Description: 
+##    Returns the inverse of the  matrix from the cache, 
+##    if the cache is null calculates the inverse, stores in the cache and
+##    then returns it
+##  Returns:
+##    The inverse of x
+##  Sample: 
+##    source("cachematrix.R")
+##    my_matrix <- makeCacheMatrix( matrix(c(4,3,3,2),2,2))
+##    cacheSolve(my_matrix)
 cacheSolve <- function(x, ...) {
   i <- x$getinversion()
   if(!is.null(i)){
@@ -30,8 +55,3 @@ cacheSolve <- function(x, ...) {
   x$setinversion(i)
   i
 }
-
-
-#source("cachematrix.R")
-#my_matrix <- makeCacheMatrix( matrix(c(4,3,3,2),2,2))
-#cacheSolve(my_matrix)
